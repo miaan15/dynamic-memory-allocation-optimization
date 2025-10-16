9c099b9788d9b115128de1257dfa61b827e45139
miaan15 <miaanstarman@gmail.com>
2025-10-16 21:08:52 +0700

    basic demo

---
+++ demo/basic/main.cpp
@@ -0,0 +1,27 @@
+
+#include <Eigen/Dense>
+#include <iostream>
+#include <toml++/toml.hpp>
+
+auto main() -> int {
+    Eigen::Matrix2d m;
+    m << 1, 2, 3, 4;
+    std::cout << "Eigen matrix:\n" << m << '\n';
+
+    auto tbl = toml::parse(R"(
+        [library]
+        name = "toml++"
+        authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+    )");
+
+    std::cout << "\nTOML table:\n" << tbl << '\n';
+
+    if (auto name = tbl["library"]["name"].value<std::string_view>()) {
+        std::cout << "TOML library name: " << *name << '\n';
+    } else {
+        std::cout << "TOML library name not found\n";
+    }
+
+    return 0;
+}
+
--- demo/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "dmao.hpp"
-
-int main() {
-    std::cout << "Hello, DMAO!" << std::endl;
-    return 0;
-}
